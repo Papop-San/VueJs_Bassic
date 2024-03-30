68f718c637ff6d4f6e44ed7dfc5817a49da34618
Papop-San <008khun159@.com>
2024-03-31 00:46:14 +0700

    update Api

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Mybar from './components/Mybar.vue';
+import Mycard from './components/Mycard.vue'
+</script>
+
+<template>
+  <header>
+    <Mybar/>
+  </header>
+  <main>
+    <Mycard/>
+  </main>
+ 
+</template>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/Mycard.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div class="card">
+        <img src="https://www.melivecode.com/attractions/1.jpg" alt="Phi Phi Islands" style="width:100%">
+        <div class="container">
+            <h4><b>Phi Phi Islands</b></h4>
+            <p>Phi Phi Islands are a group of islands in Thailand between the large island of Phuket and the Malacca Coastal Strait of Thailand.</p> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    width: 40%;
+}
+
+.card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+    padding: 2px 16px;
+}
+</style>
